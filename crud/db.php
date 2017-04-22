a5df59410f3267eca3e56b0dc2da7f393b7c7c5e
Shuntaro Tomiuchi <shuntaro103@gmail.com>
2017-04-22 22:31:46 +0900

    20170422 マージ後push

---
+++ crud/db.php
@@ -0,0 +1,27 @@
+<?php
+class DB{
+	private  $USER = "root";
+	private  $PW   = "";
+	private  $dns  = "mysql:dbname=salesmanagement;host=localhost;charset=utf8";
+
+	private function Connectdb(){
+		try{
+			$pdo = new PDO($this->dns,$this->USER,$this->PW);
+			return $pdo;
+		}catch(Exception $e){
+			return false;
+		}
+	}
+
+	public  function executeSQL($sql,$array){
+		try{
+			if(!$pdo = $this->Connectdb())return false;
+			$stmt = $pdo->prepare($sql);
+			$stmt->execute($array);
+			return $stmt;
+		}catch(Exception $e){
+			return false;
+		}
+	}
+}
+?>
